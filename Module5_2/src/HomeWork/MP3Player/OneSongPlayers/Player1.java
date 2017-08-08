e51ceac0a94e0a38706fe41332f21e4909836166
test <diachenkoiaroslav@gmail.com>
2017-08-08 09:29:21 +0300

    update

--- Module5_2/src/HomeWork/MP3Player/OneSongPlayers/Player1.java
+++ Module5_2/src/HomeWork/MP3Player/OneSongPlayers/Player1.java
@@ -1,48 +1,71 @@
 package HomeWork.MP3Player.OneSongPlayers;
 
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.AudioTrack;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import java.io.File;
 
 public class Player1 extends OneSongPlayer {
+
+    Label playingSong = new Label();
+    final String path = "/Users/test/IdeaProjects/Songs/Shakira - Dare.mp3";
+    Media media;
+    MediaPlayer mediaPlayer;
+
     public Player1(double price, String song) {
         super(price, song);
     }
 
     public void show(Pane root) {
-        VBox mainBlock = new VBox();
-
-        Text block1 = new Text("Player #1");
-        block1.setFont(new Font(20));
-
-        String path = "file:///Users/test/IdeaProjects/Songs/ShakiraDare.mp3";
-        Media media = new Media(path);
         MediaPlayer mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setVolume(0.1);
 
-        Button block2 = new Button("Play song");
-        block2.setOnAction(e -> mediaPlayer.play());
+        VBox mainBlock = new VBox();
+        mainBlock.setPadding(new Insets (20));
+        mainBlock.setSpacing(20);
+
+        Text block1 = new Text("Player #1");
+        block1.setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 20));
+
+
+
+        HBox block2 = new HBox();
+        block2.setSpacing(10);
+        Button block2_1 = new Button("Play song");
+        block2_1.setOnAction(e -> {
+            mediaPlayer.play();
+//            playingSong.setText((String)getMedia(path).getMetadata().get("title"));
+        });
+        Button block2_2 = new Button("Pause");
+        block2_2.setOnAction(e -> mediaPlayer.pause());
+        Button block2_3 = new Button("Stop");
+        block2_3.setOnAction(e -> {
+            mediaPlayer.stop();
+            playingSong.setText(null);
+        });
+        block2.getChildren().addAll(block2_1,block2_2,block2_3);
 
         HBox block3 = new HBox();
-        Label label1 = new Label("Playing:");
-        Label label2 = new Label();
-        block3.getChildren().addAll(label1,label2);
+        block3.setSpacing(10);
+        Label block3_1 = new Label("Playing:");
+        block3.getChildren().addAll(block3_1,playingSong);
 
-        VBox block4 = new VBox();
-        Label block4_1 = new Label("Song:");
-        Label block4_2 = new Label("A song");
-        block4.getChildren().addAll(block4_1,block4_2);
-
-        mainBlock.getChildren().addAll(block1,block2,block3,block4);
+        mainBlock.getChildren().addAll(block1,block2,block3);
         root.getChildren().add(mainBlock);
+    }
+
+    public Media getMedia(String path) {
+        File file = new File(path);
+        Media media = new Media(file.toURI().toString());
+        return media;
     }
 }
--- Module5_2/src/HomeWork/Main.java
+++ Module5_2/src/HomeWork/Main.java
@@ -4,7 +4,6 @@ import HomeWork.MP3Player.OneSongPlayers.Player1;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application{
