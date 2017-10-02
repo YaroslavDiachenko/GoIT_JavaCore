2f1e466122511fa2ec0d7f42a8168f2fc7151ae9
test <diachenkoiaroslav@gmail.com>
2017-10-02 08:20:46 +0300

    Final Project initial

---
+++ Final Project/src/Program/RequestScreen.java
@@ -0,0 +1,30 @@
+package Program;
+
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class RequestScreen {
+    VBox screen = new VBox();
+
+    public RequestScreen() {
+        graphicInterface();
+        addButtonBackAnalyticsScreen();
+    }
+
+    void graphicInterface() {
+        screen.setPadding(new Insets(10));
+    }
+
+    void addButtonBackAnalyticsScreen() {
+        Button buttonBackAnalyticsScreen = new Button("Back to YouTube Analytics");
+        buttonBackAnalyticsScreen.setFocusTraversable(false);
+        buttonBackAnalyticsScreen.setOnAction(event -> {
+            Main.pane.getChildren().clear();
+            Main.pane.getChildren().add(Main.youTubeAnalytics.screen);
+        });
+        screen.getChildren().add(buttonBackAnalyticsScreen);
+    }
+
+}
