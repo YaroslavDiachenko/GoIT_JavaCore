79f3e218407c3e0d91af7298bbd311d099fba5bd
Iaroslav <diachenkoiaroslav@gmail.com>
2018-03-26 14:11:09 +0300

    update

--- Module8_2_HomeWork/src/Task4.java
+++ Module8_2_HomeWork/src/Task4.java
@@ -1,4 +1,3 @@
-package HomeWork;
 
 
 import javafx.application.Application;
@@ -9,20 +8,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.*;
 import java.math.BigInteger;
-import java.util.Scanner;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
 
 /**
 
@@ -48,12 +41,19 @@ import java.util.concurrent.FutureTask;
 
 public class Task4 extends Application {
 
-    BorderPane borderPane = new BorderPane();
-    VBox top = new VBox();
-    VBox left   = new VBox();
-    VBox center = new VBox();
-    VBox right  = new VBox();
-    HBox bottom = new HBox();
+    private BorderPane borderPane;
+
+    private Button button_load;
+    private Button button_fibonacci;
+    private Button button_save;
+    private Button button_cancel;
+
+    private TextField textfield_filepath;
+    private TextField textfield_number;
+    private TextArea textArea;
+
+    private final int coresNumber = Runtime.getRuntime().availableProcessors();
+    private ExecutorService executor = Executors.newFixedThreadPool(coresNumber);
 
     public static void main(String[] args) throws IOException {
         launch(args);
@@ -61,6 +61,29 @@ public class Task4 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        setInterface();
+        configureEvents();
+
+        primaryStage.setScene(new Scene(borderPane));
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> {
+            e.consume();
+            boolean answer = confirm("Closing program", "Are you sure you want close the program?");
+            if (answer) {
+                executor.shutdown();
+                primaryStage.close();
+            }
+        });
+    }
+
+    private void setInterface() {
+        borderPane = new BorderPane();
+
+        VBox top = new VBox();
+        VBox left = new VBox();
+        VBox center = new VBox();
+        VBox right = new VBox();
+        HBox bottom = new HBox();
 
         // top:
         Text title = new Text();
@@ -72,9 +95,9 @@ public class Task4 extends Application {
         borderPane.setTop(top);
 
         // left:
-        Label label1 = new Label("Link:");
-        Label label2 = new Label("X Number:");
-        Label label3 = new Label("Text area:");
+        Text label1 = new Text("Link:");
+        Text label2 = new Text("X Number:");
+        Text label3 = new Text("Text area:");
         left.getChildren().addAll(label1, label2, label3);
         left.setPadding(new Insets(20,5,20,10));
         left.setSpacing(15);
@@ -83,25 +106,25 @@ public class Task4 extends Application {
         borderPane.setLeft(left);
 
         // center:
-        TextField textfield_link = new TextField("files/texteditor.txt");
-        textfield_link.setFocusTraversable(false);
-        TextField textfield_number = new TextField();
+        textfield_filepath = new TextField("files/texteditor.txt");
+        textfield_filepath.setFocusTraversable(false);
+        textfield_number = new TextField();
         textfield_number.setFocusTraversable(false);
-        TextArea textArea = new TextArea();
+        textArea = new TextArea();
         textArea.setFocusTraversable(false);
         textArea.setWrapText(true);
         textArea.setMinHeight(200);
         center.setPrefWidth(400);
-        center.getChildren().addAll(textfield_link,textfield_number,textArea);
+        center.getChildren().addAll(textfield_filepath,textfield_number,textArea);
         center.setPadding(new Insets(20,5,20,5));
         center.setSpacing(5);
         borderPane.setCenter(center);
 
         // right:
-        Button button_load = new Button("Load");
-        Button button_fibonacci = new Button("Fibonacci");
-        Button button_save = new Button("Save");
-        Button button_cancel = new Button("Cancel");
+        button_load = new Button("Load");
+        button_fibonacci = new Button("Fibonacci");
+        button_save = new Button("Save");
+        button_cancel = new Button("Cancel");
         right.getChildren().addAll(button_load, button_fibonacci, button_save, button_cancel);
         right.setPadding(new Insets(20,10,20,5));
         right.setSpacing(5);
@@ -122,18 +145,14 @@ public class Task4 extends Application {
         bottom.setPadding(new Insets(10));
         bottom.setSpacing(10);
         borderPane.setBottom(bottom);
+    }
 
-
-        // actions:
-
-        final int coresNumber = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.newFixedThreadPool(coresNumber);
-
+    private void configureEvents() {
         button_load.setOnMouseClicked(event -> {
-            if (textfield_link.getText().isEmpty())
-                alert("Error", "Please input a link.");
+            if (textfield_filepath.getText().isEmpty())
+                alert("Error", "Please input path to file.");
             else {
-                String filePath = textfield_link.getText();
+                String filePath = textfield_filepath.getText();
                 try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
                     textArea.clear();
                     String line;
@@ -148,41 +167,36 @@ public class Task4 extends Application {
             }
         });
 
-        button_fibonacci.setOnMouseClicked(event -> {
-            executor.submit(() -> {
-                Platform.runLater(() -> {
-                    try {
-                        int n = Integer.parseInt(textfield_number.getText());
-                        StringBuilder stringBuilder = new StringBuilder();
-                        BigInteger t1 = BigInteger.valueOf(0);
-                        BigInteger t2 = BigInteger.valueOf(1);
-                        for (int i = 1; i <= n; ++i) {
-                            stringBuilder.append(String.valueOf(t1));
-                            if (i != n) stringBuilder.append(" + ");
-                            BigInteger sum = t1.add(t2);
-                            t1 = t2;
-                            t2 = sum;
-                        }
-                        Thread.sleep(3000);
+        button_fibonacci.setOnMouseClicked(event -> executor.submit(() -> Platform.runLater(() -> {
+            try {
+                int n = Integer.parseInt(textfield_number.getText());
+                StringBuilder stringBuilder = new StringBuilder();
+                BigInteger t1 = BigInteger.valueOf(0);
+                BigInteger t2 = BigInteger.valueOf(1);
+                for (int i = 1; i <= n; ++i) {
+                    stringBuilder.append(String.valueOf(t1));
+                    if (i != n) stringBuilder.append(" + ");
+                    BigInteger sum = t1.add(t2);
+                    t1 = t2;
+                    t2 = sum;
+                }
+                Thread.sleep(3000);
 
-                        FileWriter writer = new FileWriter(textfield_link.getText());
-                        writer.write(stringBuilder.toString());
-                        writer.flush();
-                        alert("Saving","Data has been successfully saved.");
-                    }catch (NumberFormatException e) {
-                        alert("Invalid number","Please input valid number");
-                    }catch (Exception e){
-                        alert("Error","Cannot write data to file.");
-                    }
-                });
-
-            });
-        });
+                FileWriter writer = new FileWriter(textfield_filepath.getText());
+                writer.write(stringBuilder.toString());
+                writer.flush();
+                alert("Saving","Data has been successfully saved.");
+            }catch (NumberFormatException e) {
+                alert("Invalid number","Please input valid number");
+            }catch (Exception e){
+                alert("Error","Cannot write data to file.");
+            }
+        })));
 
         button_save.setOnMouseClicked(event -> {
             try {
                 String string = textArea.getText();
-                FileWriter writer = new FileWriter(textfield_link.getText());
+                FileWriter writer = new FileWriter(textfield_filepath.getText());
                 writer.write(string);
                 writer.flush();
                 textArea.setText(null);
@@ -194,26 +208,14 @@ public class Task4 extends Application {
 
         button_cancel.setOnMouseClicked(event -> {
         });
-
-        // set primary stage:
-        primaryStage.setScene(new Scene(borderPane));
-        primaryStage.show();
-        primaryStage.setOnCloseRequest(e -> {
-            e.consume();
-            boolean answer = confirm("Closing program", "Are you sure you want close the program?");
-            if (answer) {
-                executor.shutdown();
-                primaryStage.close();
-            }
-        });
     }
 
-    public long fibonacci(int n) {
+    private long fibonacci(int n) {
         if (n <= 1) return n;
         else return fibonacci(n-1) + fibonacci(n-2);
     }
 
-    public void alert(String title, String message) {
+    private void alert(String title, String message) {
         Stage stage = new Stage();
 
         stage.initModality(Modality.APPLICATION_MODAL);
@@ -235,7 +237,7 @@ public class Task4 extends Application {
         stage.showAndWait();
     }
 
-    public boolean confirm(String title, String message) {
+    private boolean confirm(String title, String message) {
         final boolean[] answer = new boolean[1];
         Stage stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
